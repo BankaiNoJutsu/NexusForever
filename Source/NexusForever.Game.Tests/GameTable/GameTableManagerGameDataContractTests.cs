7b7de322c0db98fb7dd470550f351230c488d4ef
BankaiNoJutsu <lbegert@gmail.com>
2026-06-07 10:17:53 +0200

    Add Spell4Thresholds entry to GameTableManager and corresponding test case

--- Source/NexusForever.Game.Tests/GameTable/GameTableManagerGameDataContractTests.cs
+++ Source/NexusForever.Game.Tests/GameTable/GameTableManagerGameDataContractTests.cs
@@ -21,6 +21,7 @@ public class GameTableManagerGameDataContractTests
     [InlineData(nameof(GameTableManager.RewardRotationEssence), typeof(RewardRotationEssenceEntry), "RewardRotationEssence.tbl")]
     [InlineData(nameof(GameTableManager.RewardRotationItem), typeof(RewardRotationItemEntry), "RewardRotationItem.tbl")]
     [InlineData(nameof(GameTableManager.RewardRotationModifier), typeof(RewardRotationModifierEntry), "RewardRotationModifier.tbl")]
+    [InlineData(nameof(GameTableManager.Spell4Thresholds), typeof(Spell4ThresholdsEntry), "Spell4Thresholds.tbl")]
     [InlineData(nameof(GameTableManager.ZoneCompletion), typeof(ZoneCompletionEntry), "ZoneCompletion.tbl")]
     [InlineData(nameof(GameTableManager.GenericUnlockSet), typeof(GenericUnlockSetEntry), "GenericUnlockSet.tbl")]
     public void RuntimeRequiredTables_LoadThroughDefaultInitialise(string propertyName, Type entryType, string expectedDefaultFileName)
