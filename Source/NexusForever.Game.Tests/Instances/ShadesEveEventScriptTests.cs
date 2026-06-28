88298dea2eb9f9b5e64e1cb619286ae7351146e6
BankaiNoJutsu <lbegert@gmail.com>
2026-06-28 12:42:41 +0200

    feat: expand shades eve event script

--- Source/NexusForever.Game.Tests/Instances/ShadesEveEventScriptTests.cs
+++ Source/NexusForever.Game.Tests/Instances/ShadesEveEventScriptTests.cs
@@ -1,7 +1,11 @@
+using System.Numerics;
+using NexusForever.Database.World.Model;
 using NexusForever.Game.Abstract.Entity;
+using NexusForever.Game.Abstract.Map;
 using NexusForever.Game.Abstract.Map.Instance;
 using NexusForever.Game.Abstract.PublicEvent;
 using NexusForever.Game.Abstract.Quest;
+using NexusForever.Game.Static.Entity;
 using NexusForever.Game.Static.PublicEvent;
 using NexusForever.Game.Tests.TestSupport;
 using NexusForever.GameTable.Model;
@@ -28,7 +32,8 @@ public class ShadesEveEventScriptTests
     public void OnPublicEventPhase_FindTheFountain_UsesCurrentPlayerCount()
     {
         var script = CreateScript();
-        IPublicEvent publicEvent = CreatePublicEvent(3u, out RecordingDispatchProxy<IPublicEvent> eventProxy);
+        CreatedSimple fountain = CreateSimple();
+        IPublicEvent publicEvent = CreatePublicEvent(3u, [], out RecordingDispatchProxy<IPublicEvent> eventProxy, out _, fountain.Instance);
         script.OnLoad(publicEvent);
 
         script.OnPublicEventPhase((uint)PublicEventPhase.FindTheFountain);
@@ -36,6 +41,36 @@ public class ShadesEveEventScriptTests
         RecordingDispatchProxy<IPublicEvent>.Invocation activation = Assert.Single(eventProxy.GetInvocations(nameof(IPublicEvent.ActivateObjective)));
         Assert.Equal(PublicEventObjective.FindTheFountain, activation.Arguments[0]);
         Assert.Equal(3u, activation.Arguments[1]);
+    }
+
+    [Fact]
+    public void OnPublicEventPhase_FindTheFountain_SpawnsReviewedFountainGatheringCircle()
+    {
+        var script = CreateScript();
+        CreatedSimple fountain = CreateSimple();
+        IPublicEvent publicEvent = CreatePublicEvent(3u, [], out _, out RecordingDispatchProxy<IMapInstance> mapProxy, fountain.Instance);
+        script.OnLoad(publicEvent);
+
+        script.OnPublicEventPhase((uint)PublicEventPhase.FindTheFountain);
+
+        AssertFountainGatheringCircleModel(fountain);
+        AssertGridEntityAddedToMap(mapProxy, fountain.Instance, new Vector3(333.41528f, -871.36365f, -242.43497f));
+    }
+
+    [Fact]
+    public void OnPublicEventPhase_FindTheFountain_DoesNotDuplicateFountainGatheringCircle()
+    {
+        var script = CreateScript();
+        CreatedSimple fountain = CreateSimple();
+        IPublicEvent publicEvent = CreatePublicEvent(3u, [], out RecordingDispatchProxy<IPublicEvent> eventProxy, out RecordingDispatchProxy<IMapInstance> mapProxy, fountain.Instance);
+        script.OnLoad(publicEvent);
+
+        script.OnPublicEventPhase((uint)PublicEventPhase.FindTheFountain);
+        script.OnPublicEventPhase((uint)PublicEventPhase.FindTheFountain);
+
+        Assert.Single(eventProxy.GetInvocations(nameof(IPublicEvent.CreateEntity)));
+        Assert.Single(fountain.Proxy.GetInvocations(nameof(IWorldEntity.Initialise)));
+        Assert.Single(mapProxy.GetInvocations(nameof(IMap.EnqueueAdd)));
     }
 
     [Fact]
@@ -55,15 +90,46 @@ public class ShadesEveEventScriptTests
     public void OnPublicEventPhase_SpeakWithEttyWindsen_BroadcastsMappedAngelMessage()
     {
         IPlayer player = CreatePlayer(out IGameSession session);
+        CreatedNpc etty = CreateNpc();
         ICommunicatorMessage message = RecordingDispatchProxy<ICommunicatorMessage>.Create(out RecordingDispatchProxy<ICommunicatorMessage> messageProxy);
         var script = CreateScript((CommunicatorMessage.TheAngel5, message));
-        IPublicEvent publicEvent = CreatePublicEvent(1u, [player], out _);
+        IPublicEvent publicEvent = CreatePublicEvent(1u, [player], out _, out _, etty.Instance);
         script.OnLoad(publicEvent);
 
         script.OnPublicEventPhase((uint)PublicEventPhase.SpeakWithEttyWindsen);
 
         RecordingDispatchProxy<ICommunicatorMessage>.Invocation send = Assert.Single(messageProxy.GetInvocations(nameof(ICommunicatorMessage.Send)));
         Assert.Same(session, send.Arguments[0]);
+    }
+
+    [Fact]
+    public void OnPublicEventPhase_SpeakWithEttyWindsen_SpawnsReviewedEttyWindsen()
+    {
+        CreatedNpc etty = CreateNpc();
+        var script = CreateScript();
+        IPublicEvent publicEvent = CreatePublicEvent(1u, [], out _, out RecordingDispatchProxy<IMapInstance> mapProxy, etty.Instance);
+        script.OnLoad(publicEvent);
+
+        script.OnPublicEventPhase((uint)PublicEventPhase.SpeakWithEttyWindsen);
+
+        AssertEttyWindsenModel(etty);
+        AssertGridEntityAddedToMap(mapProxy, etty.Instance, new Vector3(340.9081f, -869.39844f, -256.90118f));
+    }
+
+    [Fact]
+    public void OnPublicEventPhase_SpeakWithEttyWindsen_DoesNotDuplicateEttyWindsen()
+    {
+        CreatedNpc etty = CreateNpc();
+        var script = CreateScript();
+        IPublicEvent publicEvent = CreatePublicEvent(1u, [], out RecordingDispatchProxy<IPublicEvent> eventProxy, out RecordingDispatchProxy<IMapInstance> mapProxy, etty.Instance);
+        script.OnLoad(publicEvent);
+
+        script.OnPublicEventPhase((uint)PublicEventPhase.SpeakWithEttyWindsen);
+        script.OnPublicEventPhase((uint)PublicEventPhase.SpeakWithEttyWindsen);
+
+        Assert.Single(eventProxy.GetInvocations(nameof(IPublicEvent.CreateEntity)));
+        Assert.Single(etty.Proxy.GetInvocations(nameof(IWorldEntity.Initialise)));
+        Assert.Single(mapProxy.GetInvocations(nameof(IMap.EnqueueAdd)));
     }
 
     [Fact]
@@ -125,17 +191,34 @@ public class ShadesEveEventScriptTests
 
     private static IPublicEvent CreatePublicEvent(uint playerCount, out RecordingDispatchProxy<IPublicEvent> eventProxy)
     {
-        return CreatePublicEvent(playerCount, [], out eventProxy);
+        return CreatePublicEvent(playerCount, [], out eventProxy, out _);
     }
 
     private static IPublicEvent CreatePublicEvent(uint playerCount, IReadOnlyList<IPlayer> players, out RecordingDispatchProxy<IPublicEvent> eventProxy)
     {
-        IMapInstance mapInstance = RecordingDispatchProxy<IMapInstance>.Create(out RecordingDispatchProxy<IMapInstance> mapProxy);
+        return CreatePublicEvent(playerCount, players, out eventProxy, out _);
+    }
+
+    private static IPublicEvent CreatePublicEvent(
+        uint playerCount,
+        IReadOnlyList<IPlayer> players,
+        out RecordingDispatchProxy<IPublicEvent> eventProxy,
+        out RecordingDispatchProxy<IMapInstance> mapProxy,
+        params IGridEntity[] createdEntities)
+    {
+        IMapInstance mapInstance = RecordingDispatchProxy<IMapInstance>.Create(out mapProxy);
+        mapProxy.SetProperty(nameof(IBaseMap.Entry), new WorldEntry { Id = 3044u });
         mapProxy.SetProperty(nameof(IMapInstance.PlayerCount), playerCount);
         mapProxy.SetMethodReturn(nameof(IMapInstance.GetPlayers), players);
 
         IPublicEvent publicEvent = RecordingDispatchProxy<IPublicEvent>.Create(out eventProxy);
         eventProxy.SetProperty(nameof(IPublicEvent.Map), mapInstance);
+        if (createdEntities.Length != 0)
+        {
+            Queue<IGridEntity> queuedEntities = new(createdEntities);
+            eventProxy.SetMethodReturnFactory(nameof(IPublicEvent.CreateEntity), () => queuedEntities.Dequeue());
+        }
+
         return publicEvent;
     }
 
@@ -166,4 +249,95 @@ public class ShadesEveEventScriptTests
         playerProxy.SetProperty(nameof(IPlayer.Session), session);
         return player;
     }
+
+    private static CreatedNpc CreateNpc()
+    {
+        INonPlayerEntity npc = RecordingDispatchProxy<INonPlayerEntity>.Create(
+            out RecordingDispatchProxy<INonPlayerEntity> npcProxy);
+        return new CreatedNpc(npc, npcProxy);
+    }
+
+    private static CreatedSimple CreateSimple()
+    {
+        ISimpleEntity simple = RecordingDispatchProxy<ISimpleEntity>.Create(
+            out RecordingDispatchProxy<ISimpleEntity> simpleProxy);
+        return new CreatedSimple(simple, simpleProxy);
+    }
+
+    private static void AssertFountainGatheringCircleModel(CreatedSimple simple)
+    {
+        RecordingDispatchProxy<ISimpleEntity>.Invocation initialise = Assert.Single(
+            simple.Proxy.GetInvocations(nameof(IWorldEntity.Initialise)));
+        EntityModel model = Assert.IsType<EntityModel>(initialise.Arguments[0]);
+        Assert.Equal(1100300058u, model.Id);
+        Assert.Equal(EntityType.Simple, model.Type);
+        Assert.Equal(64820u, model.Creature);
+        Assert.Equal((ushort)3044u, model.World);
+        Assert.Equal((ushort)0u, model.Area);
+        Assert.Equal(333.41528f, model.X);
+        Assert.Equal(-871.36365f, model.Y);
+        Assert.Equal(-242.43497f, model.Z);
+        Assert.Equal(-3.1415925f, model.Rx);
+        Assert.Equal(0f, model.Ry);
+        Assert.Equal(0f, model.Rz);
+        Assert.Equal(30327u, model.DisplayInfo);
+        Assert.Equal((ushort)0u, model.OutfitInfo);
+        Assert.Equal((ushort)219u, model.Faction1);
+        Assert.Equal((ushort)219u, model.Faction2);
+        Assert.Equal(597u, model.EntityEvent.EventId);
+        Assert.Equal(0u, model.EntityEvent.Phase);
+        Assert.Empty(model.EntityScript);
+        Assert.Empty(model.EntityStat);
+    }
+
+    private static void AssertEttyWindsenModel(CreatedNpc npc)
+    {
+        RecordingDispatchProxy<INonPlayerEntity>.Invocation initialise = Assert.Single(
+            npc.Proxy.GetInvocations(nameof(IWorldEntity.Initialise)));
+        EntityModel model = Assert.IsType<EntityModel>(initialise.Arguments[0]);
+        Assert.Equal(1100300057u, model.Id);
+        Assert.Equal(EntityType.NonPlayer, model.Type);
+        Assert.Equal(62747u, model.Creature);
+        Assert.Equal((ushort)3044u, model.World);
+        Assert.Equal((ushort)0u, model.Area);
+        Assert.Equal(340.9081f, model.X);
+        Assert.Equal(-869.39844f, model.Y);
+        Assert.Equal(-256.90118f, model.Z);
+        Assert.Equal(2.9192612f, model.Rx);
+        Assert.Equal(0f, model.Ry);
+        Assert.Equal(0f, model.Rz);
+        Assert.Equal(23053u, model.DisplayInfo);
+        Assert.Equal((ushort)7913u, model.OutfitInfo);
+        Assert.Equal((ushort)219u, model.Faction1);
+        Assert.Equal((ushort)219u, model.Faction2);
+        Assert.Equal(597u, model.EntityEvent.EventId);
+        Assert.Equal(1u, model.EntityEvent.Phase);
+        Assert.Empty(model.EntityScript);
+        Assert.Equal(2, model.EntityStat.Count);
+        Assert.Contains(model.EntityStat, stat => stat.Stat == (byte)Stat.Health && stat.Value == 1f);
+        Assert.Contains(model.EntityStat, stat => stat.Stat == (byte)Stat.Level && stat.Value == 50f);
+    }
+
+    private static void AssertGridEntityAddedToMap(
+        RecordingDispatchProxy<IMapInstance> mapProxy,
+        IGridEntity entity,
+        Vector3 expectedPosition)
+    {
+        RecordingDispatchProxy<IMapInstance>.Invocation enqueueAdd = Assert.Single(
+            mapProxy.GetInvocations(nameof(IMap.EnqueueAdd)),
+            i => ReferenceEquals(entity, i.Arguments[0]));
+        Assert.Same(entity, enqueueAdd.Arguments[0]);
+
+        IMapPosition position = Assert.IsAssignableFrom<IMapPosition>(enqueueAdd.Arguments[1]);
+        Assert.Equal(expectedPosition, position.Position);
+        Assert.Equal(3044u, position.Info.Entry.Id);
+    }
+
+    private sealed record CreatedNpc(
+        INonPlayerEntity Instance,
+        RecordingDispatchProxy<INonPlayerEntity> Proxy);
+
+    private sealed record CreatedSimple(
+        ISimpleEntity Instance,
+        RecordingDispatchProxy<ISimpleEntity> Proxy);
 }
--- Source/NexusForever.Script.Instance/EventInstance/ShadesEve/ShadesEveMainEventScript.cs
+++ Source/NexusForever.Script.Instance/EventInstance/ShadesEve/ShadesEveMainEventScript.cs
@@ -1,7 +1,10 @@
+using System.Numerics;
+using NexusForever.Database.World.Model;
 using NexusForever.Game.Abstract.Entity;
 using NexusForever.Game.Abstract.Map.Instance;
 using NexusForever.Game.Abstract.PublicEvent;
 using NexusForever.Game.Abstract.Quest;
+using NexusForever.Game.Static.Entity;
 using NexusForever.Game.Static.PublicEvent;
 using NexusForever.Script.Template;
 using NexusForever.Script.Template.Filter;
@@ -13,11 +16,35 @@ namespace NexusForever.Script.Instance.EventInstances.ShadesEve
     {
         private const uint ChoosePathVoteId = 64u;
         private const uint DefaultVoteChoice = 1u;
+        private const ushort ShadesEveWorldId = 3044;
+        private const uint ShadesEvePublicEventId = 597u;
+
+        private const uint EttyWindsenEntityId = 1100300057u;
+        private const uint EttyWindsenCreatureId = 62747u;
+        private const ushort EttyWindsenAreaId = 0;
+        private const uint EttyWindsenPhase = 1u;
+        private const uint EttyWindsenDisplayInfo = 23053u;
+        private const ushort EttyWindsenOutfitInfo = 7913;
+        private const ushort EttyWindsenFactionId = 219;
+
+        private const uint FountainGatheringCircleEntityId = 1100300058u;
+        private const uint FountainGatheringCircleCreatureId = 64820u;
+        private const ushort FountainGatheringCircleAreaId = 0;
+        private const uint FountainGatheringCirclePhase = 0u;
+        private const uint FountainGatheringCircleDisplayInfo = 30327u;
+        private const ushort FountainGatheringCircleFactionId = 219;
+
+        private static readonly Vector3 EttyWindsenPosition = new(340.9081f, -869.39844f, -256.90118f);
+        private static readonly Vector3 EttyWindsenRotation = new(2.9192612f, 0f, 0f);
+        private static readonly Vector3 FountainGatheringCirclePosition = new(333.41528f, -871.36365f, -242.43497f);
+        private static readonly Vector3 FountainGatheringCircleRotation = new(-3.1415925f, 0f, 0f);
 
         private readonly IGlobalQuestManager globalQuestManager;
 
         private IPublicEvent publicEvent;
         private IMapInstance mapInstance;
+        private bool ettyWindsenSpawned;
+        private bool fountainGatheringCircleSpawned;
 
         public ShadesEveMainEventScript(
             IGlobalQuestManager globalQuestManager)
@@ -34,6 +61,8 @@ namespace NexusForever.Script.Instance.EventInstances.ShadesEve
             mapInstance = publicEvent.Map as IMapInstance
                 ?? throw new InvalidOperationException("Shades Eve requires a map instance.");
 
+            ettyWindsenSpawned = false;
+            fountainGatheringCircleSpawned = false;
             publicEvent.SetPhase(PublicEventPhase.FindTheFountain);
         }
 
@@ -46,10 +75,12 @@ namespace NexusForever.Script.Instance.EventInstances.ShadesEve
             {
                 case PublicEventPhase.FindTheFountain:
                     publicEvent.ActivateObjective(PublicEventObjective.FindTheFountain, mapInstance.PlayerCount);
+                    SpawnReviewedFountainGatheringCircle();
                     break;
                 case PublicEventPhase.SpeakWithEttyWindsen:
                     BroadcastWipCommunicatorMessage(CommunicatorMessage.TheAngel5);
                     publicEvent.ActivateObjective(PublicEventObjective.SpeakWithEttyWindsen);
+                    SpawnReviewedEttyWindsen();
                     break;
                 case PublicEventPhase.SpeakWithTheMayor:
                     publicEvent.ActivateObjective(PublicEventObjective.SpeakWithTheMayor);
@@ -104,6 +135,113 @@ namespace NexusForever.Script.Instance.EventInstances.ShadesEve
             ICommunicatorMessage communicatorMessage = globalQuestManager.GetCommunicatorMessage(message);
             foreach (IPlayer player in mapInstance.GetPlayers())
                 communicatorMessage?.Send(player.Session);
+        }
+
+        private void SpawnReviewedFountainGatheringCircle()
+        {
+            if (fountainGatheringCircleSpawned)
+                return;
+
+            // Build 16042 reviewed LaughingWS instance entity 1100300058 binds
+            // this Simple gathering circle to event 597 phase 0. Objective
+            // gather cleanup remains blocked pending client smoke.
+            ISimpleEntity entity = publicEvent.CreateEntity<ISimpleEntity>();
+            entity.Initialise(CreateFountainGatheringCircleEntityModel());
+            AddToMap(entity, FountainGatheringCirclePosition);
+            fountainGatheringCircleSpawned = true;
+        }
+
+        private static EntityModel CreateFountainGatheringCircleEntityModel()
+        {
+            return new EntityModel
+            {
+                Id          = FountainGatheringCircleEntityId,
+                Type        = EntityType.Simple,
+                Creature    = FountainGatheringCircleCreatureId,
+                World       = ShadesEveWorldId,
+                Area        = FountainGatheringCircleAreaId,
+                X           = FountainGatheringCirclePosition.X,
+                Y           = FountainGatheringCirclePosition.Y,
+                Z           = FountainGatheringCirclePosition.Z,
+                Rx          = FountainGatheringCircleRotation.X,
+                Ry          = FountainGatheringCircleRotation.Y,
+                Rz          = FountainGatheringCircleRotation.Z,
+                DisplayInfo = FountainGatheringCircleDisplayInfo,
+                Faction1    = FountainGatheringCircleFactionId,
+                Faction2    = FountainGatheringCircleFactionId,
+                EntityEvent = new EntityEventModel
+                {
+                    EventId = ShadesEvePublicEventId,
+                    Phase   = FountainGatheringCirclePhase
+                }
+            };
+        }
+
+        private void SpawnReviewedEttyWindsen()
+        {
+            if (ettyWindsenSpawned)
+                return;
+
+            // Build 16042 reviewed LaughingWS instance entity 1100300057 places
+            // Etty Windsen in event 597 phase 1 with source health/level
+            // overrides. Exact TalkTo/cinematic timing remains smoke-blocked.
+            INonPlayerEntity entity = publicEvent.CreateEntity<INonPlayerEntity>();
+            entity.Initialise(CreateEttyWindsenEntityModel());
+            AddToMap(entity, EttyWindsenPosition);
+            ettyWindsenSpawned = true;
+        }
+
+        private static EntityModel CreateEttyWindsenEntityModel()
+        {
+            return new EntityModel
+            {
+                Id          = EttyWindsenEntityId,
+                Type        = EntityType.NonPlayer,
+                Creature    = EttyWindsenCreatureId,
+                World       = ShadesEveWorldId,
+                Area        = EttyWindsenAreaId,
+                X           = EttyWindsenPosition.X,
+                Y           = EttyWindsenPosition.Y,
+                Z           = EttyWindsenPosition.Z,
+                Rx          = EttyWindsenRotation.X,
+                Ry          = EttyWindsenRotation.Y,
+                Rz          = EttyWindsenRotation.Z,
+                DisplayInfo = EttyWindsenDisplayInfo,
+                OutfitInfo  = EttyWindsenOutfitInfo,
+                Faction1    = EttyWindsenFactionId,
+                Faction2    = EttyWindsenFactionId,
+                EntityEvent = new EntityEventModel
+                {
+                    EventId = ShadesEvePublicEventId,
+                    Phase   = EttyWindsenPhase
+                },
+                EntityStat =
+                {
+                    new EntityStatModel
+                    {
+                        Stat  = (byte)Stat.Health,
+                        Value = 1f
+                    },
+                    new EntityStatModel
+                    {
+                        Stat  = (byte)Stat.Level,
+                        Value = 50f
+                    }
+                }
+            };
+        }
+
+        private void AddToMap(IGridEntity entity, Vector3 position)
+        {
+            mapInstance.EnqueueAdd(entity, new ScriptMapPosition
+            {
+                Info = new ScriptMapInfo
+                {
+                    Entry   = mapInstance.Entry,
+                    MapLock = mapInstance.MapLock
+                },
+                Position = position
+            });
         }
 
         private void SendWipCommunicatorMessage(IPlayer player, CommunicatorMessage message)
